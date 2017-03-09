831defaae04e820b3d89aa08cd394630fe8e15ec
thu <thufelixc@gmail.com>
2017-03-09 12:56:41 +0800

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+import cv2
+
+vc = cv2.VideoCapture('/Users/daiyanxu/Desktop/project/GraduationDesign/imageweuse/fight/fightgl.mp4') #读入视频文件
+c=1
+if vc.isOpened(): #判断是否正常打开
+    rval , frame = vc.read()
+else:
+    rval = False
+
+timeF = 1  #视频帧计数间隔频率
+
+while rval:   #循环读取视频帧
+    rval, frame = vc.read()
+    if(c%timeF == 0): #每隔timeF帧进行存储操作
+        cv2.imwrite('./image/'+str(c) + '.jpg',frame) #存储为图像
+    c = c + 1
+
+vc.release()
