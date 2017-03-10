614a10aacb616c912a8207e12467ed99b584453d
thu <thufelixc@gmail.com>
2017-03-10 10:34:44 +0800

    获取图片文件名

--- getName.py
+++ getName.py
@@ -2,6 +2,8 @@
 # 用于生成给yolo的图片路径文件
 import os
 
+IMAGE_PATH = '/Users/daiyanxu/Desktop/project/GraduationDesign/frame-by-frame/image'
+
 # 递归删除.DS_Store
 def walk(path):
 	for item in os.listdir(path):
@@ -14,20 +16,25 @@ def walk(path):
 		except OSError,e:
 			print e
 
-walk(VIDEO_PATH) #递归删除.DS_Store
+walk(IMAGE_PATH) #递归删除.DS_Store
 
 roots = []#所有文件夹名
 dirs = []#视频文件
 files = []#视频文件地址
 
 # 遍历
-for root, dir_, file in os.walk(VIDEO_PATH):
+for root, dir_, file in os.walk(IMAGE_PATH):
 	roots.append(root)
 	dirs.append(dir_)
 	files.append(file)
 
+# 打开一个文件
+f = open('testImagePath.txt','w')
+
 # 写入
 for i in range(6):
 	for index, file in enumerate(files[i+1]):
-		print index
-		frameByFrame(roots[i+1] + '/' + file, dirs[0][i], index)
+		f.write(roots[i+1] + '/' + file + '\n')
+
+# 关闭
+f.close()
